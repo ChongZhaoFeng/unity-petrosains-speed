83baedbb83b4ce294c08f0ab1ca9e70819ca6ce4
ZhaoFeng Chong <zfchong009@gmail.com>
2021-01-24 16:05:25 +0800

    Organized the files

--- Petrosains Speed/Assets/entergameCollide.cs
+++ Petrosains Speed/Assets/entergameCollide.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Hi : MonoBehaviour
+public class entergameCollide : MonoBehaviour
 {
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("MainMenu");
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
