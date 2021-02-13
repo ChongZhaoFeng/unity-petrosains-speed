fe097943b3516ae95cff12cec8281f8de9894ad3
Benedict <52146859+BennyS-png@users.noreply.github.com>
2021-02-14 01:14:05 +0800

    1.8

--- Petrosains Speed/Assets/Player Car & Bus/Scripts/GameController.cs
+++ Petrosains Speed/Assets/Player Car & Bus/Scripts/GameController.cs
@@ -22,11 +22,16 @@ public class GameController : MonoBehaviour
     public GameObject carMove1;
     public GameObject canMove;
     public GameObject stopSign;
+    public GameObject goodJob;
+    public GameObject barrier1;
+    public GameObject barrier2;
 
     public bool start = false; 
     public bool start2 = false;
     public bool start3 = false;
+    public bool start4 = false;
     public bool alreadyPlayed = false;
+    public bool alreadyPlayed1 = false;
 
 
     public bool pGame = false;
@@ -34,11 +39,23 @@ public class GameController : MonoBehaviour
     public bool gGame = false;
     
 
+    public bool pGame1 = false;
+    public bool cGame1 = false;
+    public bool gGame1 = false;
+
+    public bool pGame2 = false;
+    public bool cGame2 = false;
+    public bool gGame2 = false;
+
+
+
+
 
 
     public string[] time_store;
 
     public AudioSource CarHorn;
+    public AudioSource tyreNoise;
 
 
     public IEnumerator CountDown()
@@ -117,11 +134,6 @@ public class GameController : MonoBehaviour
             cGame = true;
         }
 
-        if (pGame == true)
-        {
-            Time.timeScale = 0f;
-            cGame = true;
-        }
 
         if (cGame == true)
         {
@@ -135,16 +147,16 @@ public class GameController : MonoBehaviour
         if (gGame == true)
         {
             stopSign.SetActive(false);
+            goodJob.SetActive(false);
             carMove.SetActive(false);
             carMove1.SetActive(false);
             start2 = false;
-            time.text = "00:00:00";
-            reactText.text = "Reaction three";
-
-
             Time.timeScale = 1f;
+
             
         }
+
+        
 
 
 
@@ -155,6 +167,8 @@ public class GameController : MonoBehaviour
 
             if (Input.GetKey(KeyCode.Space))
             {
+                stopSign.SetActive(false);
+                goodJob.SetActive(true);
                 start2 = false;
                 pGame = true;
                 
@@ -193,10 +207,129 @@ public class GameController : MonoBehaviour
 
         }
 
+        ////////////////////////////////////////////////////////////////////////////////////////
+
+        if (pGame1 == true)
+        {
+            
+            goodJob.SetActive(true);
+            Time.timeScale = 0f;
+            cGame1 = true;
+        }
+
+
+        if (cGame1 == true)
+        {
+            if (Input.GetKey(KeyCode.C))
+            {
+                gGame1 = true;
+                barrier1.SetActive(false);
+            }
+        }
+
+        if (gGame1 == true)
+        {
+            
+            start3 = false;
+            goodJob.SetActive(false);
+            
+            Time.timeScale = 1f;
+
+        }
+       
+
+
+
         if (start3)
         {
 
             milli += Time.deltaTime * 100;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                start3 = false;
+                pGame1 = true;
+
+            }
+
+
+
+            if (milli >= 100)
+            {
+                milli = 0;
+                sec += 1;
+            }
+            if (sec >= 60)
+            {
+                sec = 0;
+                min += 1;
+            }
+            milliDisplay = "" + milli.ToString("F1");
+
+            if (sec < 10)
+            {
+                secDisplay = "0" + sec.ToString();
+            }
+            else
+            {
+                secDisplay = sec.ToString();
+            }
+            if (min < 10)
+            {
+                minDisplay = "0" + min.ToString();
+            }
+            else
+            {
+                minDisplay = min.ToString();
+            }
+            time.text = minDisplay + ":" + secDisplay + ":" + milliDisplay;
+
+
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////
+
+        if (pGame2 == true)
+        {
+
+            goodJob.SetActive(true);
+            Time.timeScale = 0f;
+            cGame2 = true;
+        }
+
+
+        if (cGame2 == true)
+        {
+            if (Input.GetKey(KeyCode.C))
+            {
+                gGame2 = true;
+            }
+        }
+
+        if (gGame2 == true)
+        {
+            barrier2.SetActive(false);
+            start4 = false;
+            goodJob.SetActive(false);
+            Time.timeScale = 1f;
+
+        }
+
+
+
+
+        if (start4)
+        {
+
+            milli += Time.deltaTime * 100;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                start4 = false;
+                pGame2 = true;
+
+            }
+
 
 
             if (milli >= 100)
@@ -235,7 +368,6 @@ public class GameController : MonoBehaviour
 
 
 
-
     }
 
 
@@ -268,12 +400,45 @@ public class GameController : MonoBehaviour
                 CarHorn.Play();
                 alreadyPlayed = true;
             }
-            
+
+            barrier1.SetActive(true);
+
             start3 = true;
+
+        }
+
+        if (col.GetComponent<Collider>().tag == "Wall6")
+        {
+            if (!alreadyPlayed1)
+            {
+                tyreNoise.Play();
+                alreadyPlayed1 = true;
+            }
+
+            barrier2.SetActive(true);
+
+            start4 = true;
 
 
         }
 
+        if (col.GetComponent<Collider>().tag == "Wall4")
+        {
+            reactText.text = "Reaction three";
+            time.text = "00:00:00";
+        }
+
+        if (col.GetComponent<Collider>().tag == "Wall5")
+        {
+            reactText.text = "Reaction four";
+            time.text = "00:00:00";
+        }
+
+        if (col.GetComponent<Collider>().tag == "Wall7")
+        {
+            reactText.text = "Reaction five";
+            time.text = "00:00:00";
+        }
 
 
     }
